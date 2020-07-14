80e85b096afa8b308129947da8bce7a7c32e3a13
naeglinghaff <kate.o.haffenden@gmail.com>
2020-07-14 12:20:04 +0100

    refactored user unit tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe User, type: :model do
-  it "is valid with an email address and password" do
-    expect(User.new(email: "countcatula@email.com", password: "12345678")).to be_valid
+
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
   end
 
-  it 'is not valid without a valid email address' do
-    expect(User.new(email: "countcatula.email.com", password: "12345678")).to_not be_valid
-  end
-
-  it 'is not valid without a valid password' do
-    expect(User.new(email: "countcatula.email.com", password: "1234")).to_not be_valid
+  describe 'associations' do
+    it { should have_many(:tickets) }
   end
 
 end
